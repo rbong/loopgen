21ef3cb2523ea23b3331b80ff9f7658e7e08ddb4
rbong <roger.bongers@yahoo.ca>
2015-06-05 15:49:54 -0400

    First commit.

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # loopgen
 Tool for creating ordered graphviz circo loops.
+Generates a plain graphviz circo loop then hardcodes it and adds to it.
+
+# usage
+loopgen.sh input > output.dot
+
+# input file format
+```
+x number of nodes
+prefixes for the generated file (format information, labels)
+(blank)
+postfixes for the final file (extra connections, inputscale)
+```
+
+# output
+graph with node0 to nodex
+
+# example
+input file
+```
+6
+layout=circo;
+node0 [label="bop it"];
+node1 [label="twist it"];
+node2 [label="pull it"];
+node3 [label="flick it"];
+node4 [label="spin it"];
+node5 [label="throw it away"];
+
+node2 -> node5 [constraint=false,weight=0];
+// this keeps the program from running out of memory
+inputscale=72
+```
+command
+```
+loopgen.sh input | neato -Tpng > output.png
+```
+output
+![fullscreen-mode](https://raw.github.com/rbong/loopgen/master/img/screen1.png)
+normally, this would result in
+![fullscreen-mode](https://raw.github.com/rbong/loopgen/master/img/screen1.png)
---
+++ loopgen.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+# loopgen.sh- generates a plain graphviz loop then hardcodes it and adds to it
+#
+# usage-
+# loopgen.sh input > output.dot
+#
+# input file format-
+#   x num of nodes
+#   prefixes for the generated file (format information, labels)
+#   (blank)
+#   postfixes for the final file (extra connections, inputscale)
+#
+# output - graph with node0 to nodex
+file=$(<$1)
+# trim filename to function name
+fun=${1##*/}
+fun=${fun%%.*}
+# gen is generation function
+gen="digraph $fun
+{
+    layout=circo;"
+# fin is final function
+fin=""
+# get the number of inputs
+num=$(head -n 1 <<< "$file")
+if [ $num -lt 2 ]; then
+    echo "Bad number of inputs."
+    exit -1
+fi
+# increment the lines of the file
+file=$(tail -n +2 <<< "$file")
+# add all lines up to the first blank line
+gen="$gen
+    $(printf "$file" | awk '!p;/^$/{p=1}')"
+# remove all lines before the first blank line
+file=$(printf "$file" | awk '/^$/{p=1}p')
+# begin producing character-based nodes
+i=1
+gen="$gen
+    node0 "
+while [ $i -lt $num ]; do
+    # awk command gets character from number
+    gen="$gen -> node$i"
+    let i=i+1
+done
+#finish the loop
+gen="$gen -> node0;
+}"
+# generate, replace circo layout reference, make positions absolute
+gen=$(printf "$gen" | circo |
+sed -e 's/layout=circo/layout=neato/' -e 's/\(pos=".*\)"/\1!"/g')
+# remove trailing brace
+gen=$(printf "$gen" | head -n -1)
+post="/* generated with loopgen.sh */
+    $gen
+    $file
+}"
+
+printf "$post"
